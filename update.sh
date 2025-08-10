0d93f289ac882dbce331b0a0604d50fa6c2e3232
Daazed McFarland <daazedjmcfarland@gmail.com>
2025-08-10 04:39:12 -0700

    submodule script stuff

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
+#!/bin/env bash
+git submodule foreach 'branch=$(git config -f $toplevel/.gitmodules submodule.$name.branch); git checkout $branch; git pull origin $branch'
 git submodule update --recursive --remote --force
