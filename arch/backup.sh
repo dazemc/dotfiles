ea91e38a6783277b3881581b89412b50dcf2531a
Daazed J McFarland <daazedjmcfarland@gmail.com>
2026-03-06 18:55:45 -0800

    aur script

--- arch/backup.sh
+++ arch/backup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rm pkglist.txt foreignpkglist.txt
 pacman -Qqe >pkglist.txt && pacman -Qqem >foreignpkglist.txt
+sed -i '/^aura/d' ./foreignpkglist.txt
--- arch/install.sh
+++ arch/install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort pkglist.txt))
+curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/aura.tar.gz
+tar zxvf aura.tar.gz && cd aura
+makepkg -i && cd ../
+aura -A $(cat foreignpkglist.txt) --skippgpcheck
