5a1f2b1f292729cca076842184672a026faaf8e1
Daazed J McFarland <daazedjmcfarland@gmail.com>
2026-08-29 19:32:28 -0700

    hypridle

--- hyprland/hypr/conf/autostart.lua
+++ hyprland/hypr/conf/autostart.lua
@@ -9,6 +9,7 @@
 --
 local programs = require("conf.programs")
 hl.on("hyprland.start", function()
+  hl.exec_cmd("hypridle")
   hl.exec_cmd("nm-applet")
   hl.exec_cmd("awww-daemon &")
   hl.exec_cmd("aww-img /home/daze/Pictures/night_city.gif &")
